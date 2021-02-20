9cc7f10409988f45e44eeacc2db915444b564ac7
Ben <bschreiber91@cs.huji.ac.il>
2021-02-20 20:12:30 +0200

    Player.py expansion

--- player.py
+++ player.py
@@ -1,8 +1,13 @@
 class Player:
 
-    def __init__(self, id: int):
-        self.id = id
+    USER_PLAYER = 0
+    CPU_PLAYER = 1
+
+    def __init__(self, _id: int, player_type: int = Player.USER_PLAYER):
+        self.id = _id
         self.board: Board = None
+        self.score: int = 0
+        self.player_type: str = player_type
     
     def get_id(self) -> int:
         return self.id
@@ -14,3 +19,23 @@ class Player:
             deck: The deck (of type Deck) to draw from
         """
         self.board = Board(deck)
+
+    def get_score(self) -> int:
+        """
+        Returns the player's score for the course of the game
+        """
+        return self.score
+
+    def __user_turn(self):
+        pass
+
+    def __cpu_turn(self):
+        pass
+
+    def play_turn(self):
+        if self.player_type == Player.USER_PLAYER:
+            self.__user_turn()
+        elif self.player_type == Player.CPU_PLAYER:
+            self.__cpu_turn()
+
+    
